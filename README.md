84955c5a3ccb2f152113388cd0a773ac1693401b
DrorSC <sametcoh@gmail.com>
2018-11-04 00:08:08 +0200

    stop game in crash

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Simple Breakdown game in pure JavaScript
 
 Check it at work on Github Pages in the follow link:
 https://drorsc.github.io/BreakDownJS/
+
+Use arrows to move!
--- main.js
+++ main.js
@@ -1,34 +1,12 @@
 
-var myGamePiece;
 var player;
 var ball;
 
 function startGame() {
     myGameArea.start();
     ball = new ballObj(300, 300, 5);
-    myGamePiece = new component(30, 30, "red", 10, 120);
-    window.addEventListener("keydown", movePlayer, false);
-}
-
-function movePlayer(e) {
-    switch(e.keyCode){
-        case 37:
-        //left key pressed
-        myGamePiece.speedX -= 1;
-        break;
-        case 38:
-        //up key pressed
-        myGamePiece.speedY -= 1;
-        break;
-        case 39:
-        //right key pressed
-        myGamePiece.speedX += 1;
-        break;
-        case 40:
-        //down key pressed
-        myGamePiece.speedY += 1;
-        break;
-    }
+    myGamePiece = new component(50, 10, "black", 400, 500);
+    myBricks = new component(50, 20, "blue", 50, 100);
 }
 
 function component(width, height, color, x, y) {
@@ -43,20 +21,35 @@ function component(width, height, color, x, y) {
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    this.newPos = function() {
+    this.newPos = function () {
         this.x += this.speedX;
         this.y += this.speedY;
     }
+    this.crashWidth = function (otherObj) {
+        var myleft = this.x;
+        var myright = this.x + this.width;
+        var mytop = this.y;
+        var mybottom = this.y + this.height;
+        var otherleft = otherObj.x;
+        var otherright = otherObj.x + otherObj.width;
+        var othertop = otherObj.y;
+        var otherbottom = otherObj.y + otherObj.height;
+        var crash = true;
+        if ((mybottom < othertop) || mytop > otherbottom || myright < otherleft || myleft > otherright) {
+            crash = false;
+        }
+        return crash;
+    }
 }
 
-function ballObj(x, y, r){
+function ballObj(x, y, r) {
     this.x = x;
     this.y = y;
     this.r = r;
-    
-    this.fill = function(ctx){
+
+    this.fill = function (ctx) {
         ctx.begingPath();
-        ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
         ctx.fill();
     }
 }
@@ -69,14 +62,36 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = true;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = false;
+        })
     },
-    clear : function() {
+    clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function () {
+        clearInterval(this.interval);
     }
 }
 
 function updateGameArea() {
-    myGameArea.clear();
-    myGamePiece.newPos();
-    myGamePiece.update();
+    if (myGamePiece.crashWidth(myBricks)) {
+        myGameArea.stop();
+    } else {
+        myGameArea.clear();
+        myGamePiece.speedX = 0;
+        myGamePiece.speedY = 0;
+        if (myGameArea.keys && myGameArea.keys[37]) { myGamePiece.speedX = -10; }
+        if (myGameArea.keys && myGameArea.keys[38]) { myGamePiece.speedY = -10; }
+        if (myGameArea.keys && myGameArea.keys[39]) { myGamePiece.speedX = 10; }
+        if (myGameArea.keys && myGameArea.keys[40]) { myGamePiece.speedY = 10; }
+        myBricks.update();
+        myGamePiece.newPos();
+        myGamePiece.update();
+    }
 }
